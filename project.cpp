ee80f5699f4ed0fc7fafe6f5630809051276a0ad
Derek0ee <166514974+Derek0ee@users.noreply.github.com>
2024-04-09 20:41:07 +0800

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    char b;
+
+    while ( 1)
+    {
+        b =getchar();
+    //    cin >> b ;  // cin 的几个状态：
+       cout<<b <<endl; 
+    }
+    return 0;
+}
